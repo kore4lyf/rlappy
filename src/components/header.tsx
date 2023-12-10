d3329b7b7d743c4cd07c98eafa8a64f2ea466747
nathfavour <116535483+nathfavour@users.noreply.github.com>
2023-12-09 18:30:18 -0800

    new frontend

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,7 +7,7 @@ export const Header = ({ cart, user }: { cart: Cart; user?: User }) => {
   return (
     <header className="bg-blue p-5 w-100 flex justify-between items-center">
       <h1 className="text-yellow text-4xl font-bold leading-[28px]">
-        <span className="text-white">co</span>ducks
+        <span className="text-white">Rlappy</span>
       </h1>
       <NavigationMenu.Root className="">
         <NavigationMenu.List className="flex gap-[10px] items-center justify-between text-xl">
