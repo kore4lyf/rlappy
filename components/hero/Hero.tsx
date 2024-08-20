d1989295bf0f060dd1c7f04cf03b6af0ab54cacd
Korede Faleye <akfaleye@gmail.com>
2024-08-20 13:42:15 +0100

    Hide hero image on tablets

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -10,20 +10,22 @@ const numbAction: React.MouseEventHandler<HTMLImageElement> = (e) => e.preventDe
 
 const Hero: React.FC = () => {
   return (
-    <section className="autoWidth hero min-h-screen">
-      <div className="flex flex-col lg:flex-row justify-start ">
-        <div className="mb-10 relative w-full hidden sm:block">
-            <Image alt="Image of a Laptop" unoptimized={true} src={heroImg} draggable="false" onContextMenu={numbAction}  className="max-w-full" />
-        </div>
-        <div className="w-full grid grid-flow-row">
-          <div>
-            <h1 className="text-5xl sm:text-5xl lg:text-6xl font-bold leading-tight">Unlock Your <span className="text-primary">Freedom</span></h1>
-            <p className="py-6 w-full">
-                Discover top-quality laptops for rent or list your own to start earning today. Seamless rentals, flexible terms, and guaranteed quality.
-            </p>
-            <HeroSearchForm />
+    <section className="border-b-4 border-base-300 xl:py-32">
+      <div className="autoWidth hero">
+        <div className="flex flex-col lg:flex-row justify-start ">
+          <div className="mb-10 relative w-full hidden xl:block">
+              <Image alt="Image of a Laptop" unoptimized={true} src={heroImg} draggable="false" onContextMenu={numbAction}  className="max-w-full" />
           </div>
-            <SiteStats />
+          <div className="w-full grid grid-flow-row">
+            <div>
+              <h1 className="text-5xl sm:text-5xl lg:text-6xl font-bold leading-tight">Unlock Your <span className="text-primary">Freedom</span></h1>
+              <p className="py-6 w-full">
+                  Discover top-quality laptops for rent or list your own to start earning today. Seamless rentals, flexible terms, and guaranteed quality.
+              </p>
+              <HeroSearchForm />
+            </div>
+              <SiteStats />
+          </div>
         </div>
       </div>
     </section>
